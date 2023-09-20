ee947e4d86eda3be9c1c1ba7514f527c9e713b3d
UtkarshGhadage <utkarshghadage01@gmail.com>
2023-09-21 01:15:32 +0530

    2nd

--- React/components/Header.js
+++ React/components/Header.js
@@ -1,19 +1,46 @@
 import Title from "../components/Title";
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const HeaderComponent = () => {
-    return (
-      <div className="header">
-        <Title />
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Card</li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
+  const [isLoggedin, setIsLoggedin] = useState(true);
 
-  export default HeaderComponent;
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+        <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
+          <li>
+            <i className="fa-solid fa-cart-shopping"></i>
+          </li>
+          <li>
+            {/* use conditional rendering for login and logout */}
+            {isLoggedin ? (
+              <button
+                className="logout-btn"
+                onClick={() => setIsLoggedin(false)}
+              >
+                Logout
+              </button>
+            ) : (
+              <button className="login-btn" onClick={() => setIsLoggedin(true)}>
+                Login
+              </button>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderComponent;
--- React/src/App.js
+++ React/src/App.js
@@ -4,6 +4,12 @@ import ReactDOM from "react-dom/client";
 import HeaderComponent from "../components/Header";
 import Body from "../components/Body"
 import Footer from "../components/Footer"
+import About from "../components/About";
+import Error from "../components/Error";
+import RestaurantMenu from "../components/RestaurantMenu";
+import { Contact } from "../components/Contact";
+
+import {RouterProvider, createBrowserRouter, Outlet} from "react-router-dom"
 
 
 /**
@@ -22,15 +28,43 @@ import Footer from "../components/Footer"
         3. FOOTER
       **/
 
+
+
+
 const AppLayout = () => {
   return (
     <>
       <HeaderComponent />
-      <Body />
+      <Outlet/>
       <Footer />
     </>
   );
 };
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout/>,
+    errorElement: <Error/>,
+    children:[{
+      path: "/",
+      element: <Body/>
+    },{
+      path: "/about",
+      element: <About/>
+    },{
+      path: "/contact",
+      element: <Contact/>
+    },{
+      path: "/restaurant/:id",
+      element: <RestaurantMenu/>
+    }
+  ]
+  }
+  
+])
+
 
 
 
@@ -39,4 +73,4 @@ const AppLayout = () => {
 const root = ReactDOM.createRoot(document.getElementById("root")); //when we will be modifying data on DOM then need to use ReactDOM
 
 //root.render(heading); // we are putting a react element inside root
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
